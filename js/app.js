3e8d597006e8a052c59dedff2e3175fcfa9f2a56
cuipilao <cuipilao@users.noreply.github.com>
2026-07-30 20:49:13 +0800

    v12: 数学删除分类tab统一加减混合+全部红色tab+放大题目字号

--- js/app.js
+++ js/app.js
@@ -510,25 +510,16 @@
   // ====== 数学 ======
   Pages.math = function () {
     setTopbar('数学', '➕');
-    const sub = location.hash.split('/')[2] || 'mix';
-    const range = parseInt(location.hash.split('/')[3] || '10', 10);
+    const range = parseInt(location.hash.split('/')[2] || '10', 10);
     const root = el('div', null, [
       el('div', { class: 'tabs-pill' }, [
-        pillBtn('mix', '加减混合', sub, '#/math/mix/' + range),
-        pillBtn('add', '加法', sub, '#/math/add/' + range),
-        pillBtn('sub', '减法', sub, '#/math/sub/' + range)
-      ]),
-      el('div', { class: 'tabs-pill ghost', style: { marginTop: '-4px' } }, [
-        pillBtn(10, '10 以内', range, '#/math/' + sub + '/10'),
-        pillBtn(20, '20 以内', range, '#/math/' + sub + '/20')
+        pillBtn(10, '10 以内', range, '#/math/10'),
+        pillBtn(20, '20 以内', range, '#/math/20')
       ]),
       el('div', { id: 'mathBox' })
     ]);
     setTimeout(() => {
-      if (sub === 'mix') $('#mathBox').appendChild(mathQuiz('mix', range));
-      else if (sub === 'add') $('#mathBox').appendChild(mathQuiz('add', range));
-      else if (sub === 'sub') $('#mathBox').appendChild(mathQuiz('sub', range));
-      else $('#mathBox').appendChild(mathQuiz('mix', range));
+      $('#mathBox').appendChild(mathQuiz('mix', range));
     }, 0);
     return root;
   };
